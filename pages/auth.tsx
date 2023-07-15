7f6b964f8c7aee93775abc20f02b78e4fa0f78d9
rayyan21d <rayyan9290@gmail.com>
2023-07-15 16:31:36 +0530

    stage 2 of ui

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import Input from "@/components/Input";
+import { useState } from "react";
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -14,12 +19,39 @@ const Auth = () => {
 
             <div className="flex flex-col gap-4">
               <Input
-                label="Email"
-                id={""}
-                onChange={undefined}
-                value={""}
-                type={""}
+                label="Username"
+                id="name"
+                onChange={(event: any) => setName(event.target.value)}
+                value={name}
+                type=""
               />
+
+              <Input
+                label="Email"
+                id="email"
+                onChange={(event: any) => setEmail(event.target.value)}
+                value={email}
+                type="email"
+              />
+
+              <Input
+                label="Password"
+                id="password"
+                onChange={(event: any) => setPassword(event.target.value)}
+                value={password}
+                type="password"
+              />
+
+              <button className="bg-red-600 py-3 rounded-md w-full text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                Sign in
+              </button>
+
+              <p className="text-neutral-500 mt-500 mt-12">
+                Don&apos;t have an account?
+                <span className="text-white ml-1 hover:underline cursor-pointer">
+                  Create an account
+                </span>
+              </p>
             </div>
           </div>
         </div>
