6d0b960abcda1cad5d0887ffc9536b540f2d2089
rayyan21d <rayyan9290@gmail.com>
2023-07-16 22:16:10 +0530

    navbar setup

---
+++ components/NavbarItem.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+
+interface NavbarItemProps {
+  label: string;
+  active?: boolean;
+}
+
+const NavbarItem: FC<NavbarItemProps> = ({ label, active }) => {
+  return (
+    <div
+      className={
+        active
+          ? "text-white cursor-default"
+          : "text-gray-200 hover:text-gray-300 cursor-pointer transition"
+      }
+    >
+      {label}
+    </div>
+  );
+};
+
+export default NavbarItem;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { getSession, signOut } from "next-auth/react";
 import { NextPageContext, InferGetServerSidePropsType } from "next";
 import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -26,14 +27,7 @@ export default function Home(
 
   return (
     <>
-      <h1 className="text-5xl text-green-500">Netflix Clone</h1>;
-      <p className="text-white">Logged in as :{user?.name}</p>
-      <button
-        onClick={() => signOut()}
-        className="h-10 w-full bg-white hover:opacity-80"
-      >
-        Logout!
-      </button>
+      <Navbar />
     </>
   );
 }
